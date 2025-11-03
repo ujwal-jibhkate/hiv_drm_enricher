6d4fbd5d26a31cb434ded098a66b2f6bd7e9b1d3
Ujwal Jibhkate <ujwaljibhkate@gmail.com>
2025-11-03 23:45:26 +0000

    single file to run entire pipeline

--- scripts/run_final_pipeline.py
+++ scripts/run_final_pipeline.py
@@ -6,10 +6,21 @@ import argparse
 import pysam
 
 # --- FINAL CONFIGURATION & DATABASE BUILDER ---
+"""
+# This is for HXB2 coordinates
 GENE_COORDINATES = {
     "PR": {"start": 1385, "end": 1682},
     "RT": {"start": 1683, "end": 3363}
 }
+
+"""
+
+# These are the NEW, gene-relative coordinates for your pol_gene_HIV1_sequence.fasta
+GENE_COORDINATES = {
+    "PR": {"start": 1, "end": 297},       # Protease is the first 297 bases (99 codons)
+    "RT": {"start": 298, "end": 1977}     # RT (560 codons) starts right after PR
+}
+
 CODON_TABLE = {'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R', 'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
 
 def build_definitive_database(csv_path: str) -> dict:
